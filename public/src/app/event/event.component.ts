397b01a52386e53b85748c2696d527641aead335
Kaysee Weberski <kweberski@gmail.com>
2019-07-24 16:58:57 -0700

    icon change

--- public/src/app/event/event.component.ts
+++ public/src/app/event/event.component.ts
@@ -12,9 +12,11 @@ import {} from 'googlemaps';
 export class EventComponent implements OnInit {
     header = 'Events';
     map;
-    // infoWindow;
     marker;
-    // image = "http://www.pngall.com/wp-content/uploads/2017/05/Map-Marker-PNG-File.png"
+    image = {
+      url: "http://www.myiconfinder.com/uploads/iconsets/256-256-a5485b563efc4511e0cd8bd04ad0fe9e.png",
+      scaledSize: new google.maps.Size(35, 35)
+    }
 
   constructor(private _httpService: HttpService) { 
   }
@@ -27,93 +29,92 @@ export class EventComponent implements OnInit {
     this.map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: -34.397, lng: 150.644},
       zoom: 12,
-      // styles: [   // gives the map a 'night mode' appearance
-      //   {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
-      //   {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
-      //   {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
-      //   {
-      //     featureType: 'administrative.locality',
-      //     elementType: 'labels.text.fill',
-      //     stylers: [{color: '#d59563'}]
-      //   },
-      //   {
-      //     featureType: 'poi',
-      //     elementType: 'labels.text.fill',
-      //     stylers: [{color: '#d59563'}]
-      //   },
-      //   {
-      //     featureType: 'poi.park',
-      //     elementType: 'geometry',
-      //     stylers: [{color: '#263c3f'}]
-      //   },
-      //   {
-      //     featureType: 'poi.park',
-      //     elementType: 'labels.text.fill',
-      //     stylers: [{color: '#6b9a76'}]
-      //   },
-      //   {
-      //     featureType: 'road',
-      //     elementType: 'geometry',
-      //     stylers: [{color: '#38414e'}]
-      //   },
-      //   {
-      //     featureType: 'road',
-      //     elementType: 'geometry.stroke',
-      //     stylers: [{color: '#212a37'}]
-      //   },
-      //   {
-      //     featureType: 'road',
-      //     elementType: 'labels.text.fill',
-      //     stylers: [{color: '#9ca5b3'}]
-      //   },
-      //   {
-      //     featureType: 'road.highway',
-      //     elementType: 'geometry',
-      //     stylers: [{color: '#746855'}]
-      //   },
-      //   {
-      //     featureType: 'road.highway',
-      //     elementType: 'geometry.stroke',
-      //     stylers: [{color: '#1f2835'}]
-      //   },
-      //   {
-      //     featureType: 'road.highway',
-      //     elementType: 'labels.text.fill',
-      //     stylers: [{color: '#f3d19c'}]
-      //   },
-      //   {
-      //     featureType: 'transit',
-      //     elementType: 'geometry',
-      //     stylers: [{color: '#2f3948'}]
-      //   },
-      //   {
-      //     featureType: 'transit.station',
-      //     elementType: 'labels.text.fill',
-      //     stylers: [{color: '#d59563'}]
-      //   },
-      //   {
-      //     featureType: 'water',
-      //     elementType: 'geometry',
-      //     stylers: [{color: '#17263c'}]
-      //   },
-      //   {
-      //     featureType: 'water',
-      //     elementType: 'labels.text.fill',
-      //     stylers: [{color: '#515c6d'}]
-      //   },
-      //   {
-      //     featureType: 'water',
-      //     elementType: 'labels.text.stroke',
-      //     stylers: [{color: '#17263c'}]
-      //   }
-      // ]
+      styles: [   // gives the map a 'night mode' appearance
+        {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+        {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+        {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+        {
+          featureType: 'administrative.locality',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#d59563'}]
+        },
+        {
+          featureType: 'poi',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#d59563'}]
+        },
+        {
+          featureType: 'poi.park',
+          elementType: 'geometry',
+          stylers: [{color: '#263c3f'}]
+        },
+        {
+          featureType: 'poi.park',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#6b9a76'}]
+        },
+        {
+          featureType: 'road',
+          elementType: 'geometry',
+          stylers: [{color: '#38414e'}]
+        },
+        {
+          featureType: 'road',
+          elementType: 'geometry.stroke',
+          stylers: [{color: '#212a37'}]
+        },
+        {
+          featureType: 'road',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#9ca5b3'}]
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'geometry',
+          stylers: [{color: '#746855'}]
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'geometry.stroke',
+          stylers: [{color: '#1f2835'}]
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#f3d19c'}]
+        },
+        {
+          featureType: 'transit',
+          elementType: 'geometry',
+          stylers: [{color: '#2f3948'}]
+        },
+        {
+          featureType: 'transit.station',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#d59563'}]
+        },
+        {
+          featureType: 'water',
+          elementType: 'geometry',
+          stylers: [{color: '#17263c'}]
+        },
+        {
+          featureType: 'water',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#515c6d'}]
+        },
+        {
+          featureType: 'water',
+          elementType: 'labels.text.stroke',
+          stylers: [{color: '#17263c'}]
+        }
+      ]
     });
     console.log(this.map)
-    // this.infoWindow = new google.maps.InfoWindow;
     this.marker = new google.maps.Marker({
       position: this.map.center,
       map: this.map,
-      // icon: this.image
+      icon: this.image
     })
 
     if (navigator.geolocation) {                        // will pull locaiton based on current location
@@ -123,8 +124,6 @@ export class EventComponent implements OnInit {
           lng: position.coords.longitude
         };
           this.marker.setPosition(pos);
-          // this.infoWindow.setContent("Your're here!");
-          // this.infoWindow.open(this.map);
           this.map.setCenter(pos);
       }), function() {
         this.handleLocationError(true, this.infoWindow, this.map.getCenter());
@@ -137,10 +136,8 @@ export class EventComponent implements OnInit {
 
   handleLocationError(browserHasGeolocation, marker, pos) {
     marker.setPosition(pos);
-    // infoWindow.setContent(browserHasGeolocation ? 
-    //   'Error: The Geolocation service failed.' : 
-    //   'Error: Your browser doesn\'t support geolocation.');
-    // infoWindow.open(this.map);
+      console.log(browserHasGeolocation ? 'Error: The Geolocation service failed.' : 
+      'Error: Your browser doesn\'t support geolocation.');
   }
 }
 
