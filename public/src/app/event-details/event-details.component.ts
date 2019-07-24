d40ace317eb8209bcc38a71d38e3397dab5cd3ed
Kaysee Weberski <kweberski@gmail.com>
2019-07-24 16:16:21 -0700

    google maps w/ geolocation

--- public/src/app/event-details/event-details.component.ts
+++ public/src/app/event-details/event-details.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-event-details',
   templateUrl: './event-details.component.html',
   styleUrls: ['./event-details.component.css']
+
 })
 export class EventDetailsComponent implements OnInit {
 
--- public/src/app/http.service.ts
+++ public/src/app/http.service.ts
@@ -11,4 +11,5 @@ export class HttpService {
   getAll() {  // correlates with what will be passed into the home/hom.component.ts
     return this._http.get('/all'); //correlates with the routes established in the server.js
   }
+
 }
